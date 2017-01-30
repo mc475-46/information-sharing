29d886b51028c8c874102d2f495ae1f342719169
Motoki Nagano <motoki.n.310@gmail.com>
2017-01-30 23:56:49 +0900

    hoge

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from bottle import route, run, template, static_file, url
+from bottle import route, run, template, static_file, url, request
 
 
 @route('/static/<filepath:path>')
@@ -8,17 +8,52 @@ def static(filepath):
 
 @route('/')
 def home():
-    return template('template/home.html', url=url)
+    return template('template/home.html')
 
 
 @route('/person')
 def person():
-    return template('template/person.html', url=url)
+    return template('template/person.html')
+
+
+@route('/person/<search_from>')
+def search_person(search_from):
+    if search_from == "department":
+        return template('template/person/department.html')
+    elif search_from == "project":
+        return template('template/person/project.html')
+    elif search_from == "club":
+        return template('template/person/club.html')
+    elif search_from == "ryugaku":
+        return template('template/person/ryugaku.html')
+    elif search_from == "skilltree":
+        return template('template/person/skilltree.html')
+    elif search_from == "1":
+        return template('template/person/taro.html')
+    else:
+        return template('template/not_implemented.html')
+
+
+@route('/person/department/class')
+def search_person_from_class():
+    return template('template/person/class.html')
+
+
+@route('/person/department/list')
+def search_person_from_list():
+    return template('template/person/list.html')
 
 
 @route('/search')
 def search():
-    return template('template/not_implemented.html', url=url)
+    search_string = request.query.search
+    return template('template/search.html', search_string=search_string)
+
+
+@route('/not_implemented')
+def not_implemented():
+    return template('template/not_implemented.html')
+
 
 from os import environ
 if environ.get("IS_HEROKU"):
